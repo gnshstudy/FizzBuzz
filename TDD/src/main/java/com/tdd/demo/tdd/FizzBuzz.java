184db4716a96ab0a787cbc4b3064ee7994c252f6
Lenovo <Lenovo@host.docker.internal>
2022-09-22 22:25:38 +0530

    testFizz code reafactoring to work with number divisible with 3

--- TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
+++ TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class FizzBuzz {
 
 	public String play(int i) {
-		if(i==3) {
+		if(i%3==0) {
 			return "Fizz";
 		}
 		return String.valueOf(i);
