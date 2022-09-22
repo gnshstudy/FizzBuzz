a3c5fa2006f8c09c6378ea5b4bf6e8fea13cfd53
Lenovo <Lenovo@host.docker.internal>
2022-09-22 22:23:50 +0530

    testFizz Success for input 3

--- TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
+++ TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 public class FizzBuzz {
 
 	public String play(int i) {
+		if(i==3) {
+			return "Fizz";
+		}
 		return String.valueOf(i);
 		// TODO Auto-generated method stub
 	}
