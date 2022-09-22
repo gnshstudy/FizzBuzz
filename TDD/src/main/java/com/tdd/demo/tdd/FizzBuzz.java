971bbaa39e5e8d6005cd1913a0fe64eaf28af1fa
Lenovo <Lenovo@host.docker.internal>
2022-09-22 22:51:30 +0530

    when input is null expecting an exception to be thrown

--- TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
+++ TDD/src/main/java/com/tdd/demo/tdd/FizzBuzz.java
@@ -5,17 +5,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class FizzBuzz {
 
-	public String play(int i) {
-		if(i%3==0 && i%5==0) {
-			return "FizzBuzz";
-		}else if(i%3==0) {
-			return "Fizz";
-		}else if(i%5==0) {
-			return "Buzz";
-		}else {
-		return String.valueOf(i);
+	public String play(int i) throws Exception {
+		String returnValue;
+		try {
+
+			if (i % 3 == 0 && i % 5 == 0) {
+				returnValue = "FizzBuzz";
+				return "FizzBuzz";
+			} else if (i % 3 == 0) {
+				returnValue = "Fizz";
+				return "Fizz";
+			} else if (i % 5 == 0) {
+				returnValue = "Buzz";
+
+			} else {
+				returnValue = String.valueOf(i);
+			}
+		} catch (Exception e) {
+			throw new Exception("Invalid Input Value");
 		}
 		// TODO Auto-generated method stub
+		return returnValue;
 	}
 
 }
--- TDD/src/test/java/com/tdd/demo/tdd/FizzBuzzTest.java
+++ TDD/src/test/java/com/tdd/demo/tdd/FizzBuzzTest.java
@@ -1,8 +1,10 @@
 package com.tdd.demo.tdd;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
@@ -13,28 +15,41 @@ public class FizzBuzzTest {
 		fizzBuzz = new FizzBuzz();
 	}
 
+	@DisplayName("Number is not divisible by both 3 and 5")
 	@Test
-	public void testNumber() {
+	public void testNumber() throws Exception {
 		String returnedNumber = fizzBuzz.play(2);
 		assertEquals("2", returnedNumber);
 	}
 
+	@DisplayName("FizzNumber is not divisible by 3")
 	@Test
-	public void testFizz() {
+	public void testFizz() throws Exception {
 		String returnedNumber = fizzBuzz.play(6);
 		assertEquals("Fizz", returnedNumber);
 	}
 
+	@DisplayName("Buzz-Number is not divisible by 5")
 	@Test
-	public void testBuzz() {
+	public void testBuzz() throws Exception {
 		String returnedNumber = fizzBuzz.play(5);
 		assertEquals("Buzz", returnedNumber);
 	}
 
+	@DisplayName("FizzBuzz-Number is not divisible by both 3 and 5")
 	@Test
-	public void testFizzBuzz() {
+	public void testFizzBuzz() throws Exception {
 		String returnedNumber = fizzBuzz.play(15);
 		assertEquals("FizzBuzz", returnedNumber);
+	}
+
+	@DisplayName("Expecting Exception When input is null")
+	@Test
+	public void testIfNumberIsNull() throws Exception {
+		assertThrows(Exception.class, () -> {
+			fizzBuzz.play((Integer) null);
+		});
+
 	}
 
 }
